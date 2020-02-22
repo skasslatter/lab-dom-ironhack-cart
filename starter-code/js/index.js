664c673d78a6a4cd2532594f3e7b5650f1b233a5
Sybille Kasslatter <sybille.kasslatter@hotmail.com>
2020-02-22 09:14:10 +0100

    3rd iteration done

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,11 +1,27 @@
-var $cart = document.querySelector('#cart tbody');
+/////////////////////
 var $calc = document.getElementById('calc');
+let $cart = document.querySelector('#cart tbody');
+let prodList = document.querySelectorAll('.product');
 
-function updateSubtot($product) {
-  // Iteration 1.1
-}
 
-function calcAll() {
-  // Iteration 1.2
-}
+function updateSubtot ($product) {
+    let unitPrice = parseInt($product.getElementsByClassName('test')[0].innerHTML);
+    let quantity = parseInt($product.getElementsByClassName("unit")[0].value);
+    return unitPrice * quantity;
+  };
+
+
+function calcAll (){
+  debugger
+  let totalPrice = 0;
+  for (i = 0; i < prodList.length; i++){
+    prodList[i].getElementsByClassName('subtot')[0].innerHTML = updateSubtot(
+      prodList[i]
+      );
+      totalPrice += parseInt(updateSubtot(prodList[i]));
+  }
+    document.querySelector("#total span").innerHTML = totalPrice;
+  }
+
 $calc.onclick = calcAll;
+/////////////////////
